e32c8acf2235be881fbe00070b0aa1fec3f4e810
NhaXeAnhDuc <nhaxeanhduc2000@gmail.com>
2025-02-13 20:54:21 +0700

    'done'

--- public/js/dang-nhap.js
+++ public/js/dang-nhap.js
@@ -1,6 +1,8 @@
 const openlogin = document.getElementById('open_login');
 const loginform = document.getElementById('login_form');
 const buttonclose = document.getElementById('button_close');
+const usernamelogin = document.getElementById('username_login');
+const password_login = document.getElementById('password_login');
 
 openlogin.addEventListener('click', () => {
     loginform.classList.add('open_form');
@@ -9,3 +11,10 @@ openlogin.addEventListener('click', () => {
 buttonclose.addEventListener('click', () => {
     loginform.classList.remove('open_form');
 });
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+
--- public/js/trang-chu.js
+++ public/js/trang-chu.js
@@ -1,7 +1,3 @@
 document.getElementById('btn_login').addEventListener('click', () => {
-  window.location.href = '/trangchu/trangchu.html';
-});
-
-document.getElementById('search_bus').addEventListener('click', () => {
-  
-});
+  window.location.href = '/login'
+})
